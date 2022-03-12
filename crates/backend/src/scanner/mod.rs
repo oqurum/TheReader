c3f7bc91f9bc7fa057fc58a10fdc89bc6ec0ba59
Its <2779546+Its-its@users.noreply.github.com>
2022-03-12 02:49:19 -0800

    File type whitelist for scanner

--- crates/backend/src/scanner/mod.rs
+++ crates/backend/src/scanner/mod.rs
@@ -5,6 +5,10 @@ use tokio::fs;
 
 use crate::database::{NewFile, Database, Library};
 
+
+pub static WHITELISTED_FILE_TYPES: [&str; 1] = ["epub"];
+
+
 pub async fn library_scan(library: &Library, db: Database) -> Result<()> {
 	let mut folders: VecDeque<PathBuf> = std::collections::VecDeque::from([PathBuf::from(&library.path)]);
 
@@ -20,31 +24,36 @@ pub async fn library_scan(library: &Library, db: Database) -> Result<()> {
 			if file_type.is_dir() {
 				folders.push_back(path);
 			} else if file_type.is_file() {
-				let file_size = fs::read(&path).await?.len(); // TODO: Remove fs::read
 
 				let file_name = file_name.into_string().unwrap();
 				let (file_name, file_type) = match file_name.rsplit_once('.') {
-					Some((v1, v2)) => (v1.to_string(), v2.to_string()),
+					Some((v1, v2)) => (v1.to_string(), v2.to_string().to_lowercase()),
 					None => (file_name, String::new())
 				};
 
-				let file = NewFile {
-					path: path.to_str().unwrap().replace("\\", "/"),
+				if WHITELISTED_FILE_TYPES.contains(&file_type.as_str()) {
+					let file_size = fs::read(&path).await?.len(); // TODO: Remove fs::read
 
-					file_name,
-					file_type,
-					file_size: file_size as i64,
+					let file = NewFile {
+						path: path.to_str().unwrap().replace("\\", "/"),
 
-					library_id: library.id,
-					metadata_id: 0,
-					chapter_count: 0,
+						file_name,
+						file_type,
+						file_size: file_size as i64,
 
-					modified_at: meta.modified()?.elapsed()?.as_millis() as i64,
-					accessed_at: meta.accessed()?.elapsed()?.as_millis() as i64,
-					created_at: meta.created()?.elapsed()?.as_millis() as i64,
-				};
+						library_id: library.id,
+						metadata_id: 0,
+						chapter_count: 0,
 
-				db.add_file(&file)?;
+						modified_at: meta.modified()?.elapsed()?.as_millis() as i64,
+						accessed_at: meta.accessed()?.elapsed()?.as_millis() as i64,
+						created_at: meta.created()?.elapsed()?.as_millis() as i64,
+					};
+
+					db.add_file(&file)?;
+				} else {
+					log::info!("Skipping File {:?}. Not a whitelisted file type.", path);
+				}
 			}
 		}
 	}
