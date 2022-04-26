26e3a92497434c0bf3cda341dc55ba6ae2d1af48
Its <2779546+Its-its@users.noreply.github.com>
2022-04-26 02:21:05 -0700

    Updated Navbar display

--- crates/frontend/src/components/navbar.rs
+++ crates/frontend/src/components/navbar.rs
@@ -8,7 +8,8 @@ pub enum Msg {
 }
 
 pub struct NavbarModule {
-	items: Vec<(Route, &'static str)>
+	left_items: Vec<(Route, DisplayType)>,
+	right_items: Vec<(Route, DisplayType)>,
 }
 
 impl Component for NavbarModule {
@@ -17,10 +18,12 @@ impl Component for NavbarModule {
 
 	fn create(_ctx: &Context<Self>) -> Self {
 		Self {
-			items: vec![
-				(Route::Dashboard, "Home"),
-				(Route::Options, "Options")
-			]
+			left_items: vec![
+				(Route::Dashboard, DisplayType::Icon("home", "Home")),
+			],
+			right_items: vec![
+				(Route::Options, DisplayType::Icon("settings", "Settings")),
+			],
 		}
 	}
 
@@ -31,12 +34,43 @@ impl Component for NavbarModule {
 	fn view(&self, _ctx: &Context<Self>) -> Html {
 		html! {
 			<div class="navbar-module">
+				<div class="left-content">
 				{
-					for self.items.iter().map(|item| html! {
-						<Link<Route> to={item.0.clone()}>{ item.1 }</Link<Route>>
-					})
+					for self.left_items.iter().map(|item| Self::render_item(item.0.clone(), &item.1))
 				}
+				</div>
+				<div class="center-content">
+					<div class="search-bar">
+						<input placeholder="Search" />
+						<button>{ "Search" }</button>
+					</div>
+				</div>
+				<div class="right-content">
+				{
+					for self.right_items.iter().map(|item| Self::render_item(item.0.clone(), &item.1))
+				}
+				</div>
 			</div>
 		}
 	}
+}
+
+impl NavbarModule {
+	fn render_item(route: Route, name: &DisplayType) -> Html {
+		match name {
+			DisplayType::Text(v) => html! {
+				<Link<Route> to={route}>{ v }</Link<Route>>
+			},
+			DisplayType::Icon(icon, title) => html! {
+				<Link<Route> to={route}>
+					<span class="material-icons" title={ *title }>{ icon }</span>
+				</Link<Route>>
+			}
+		}
+	}
+}
+
+pub enum DisplayType {
+	Text(&'static str),
+	Icon(&'static str, &'static str),
 }
