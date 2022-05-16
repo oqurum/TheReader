d42c124f95d9420cf01b73db8b2327751ebee222
Its <2779546+Its-its@users.noreply.github.com>
2022-05-16 11:24:59 -0700

    Deletions

--- crates/backend/src/config.rs
+++ crates/backend/src/config.rs
@@ -7,33 +7,6 @@ use serde::{Serialize, Deserialize};
 use crate::Result;
 
 
-// <?xml version="1.0" encoding="utf-8"?>
-// <Preferences
-// 	MachineIdentifier="c80610b4-1c7e-42a0-a345-ad762bd7a9a9"
-// 	ProcessedMachineIdentifier="221a00fbe4d1d9771af241aeb7e3a06d695d8443"
-// 	TranscoderTempDirectory="/transcode"
-// 	OldestPreviousVersion="legacy"
-// 	AnonymousMachineIdentifier="30b001d0-4dfe-4e2b-ba59-3b994c2e8ba1"
-// 	MetricsEpoch="1"
-// 	AcceptedEULA="1"
-// 	FriendlyName="Media 2"
-// 	PublishServerOnPlexOnlineKey="1"
-// 	PlexOnlineToken="TR5RE7xBMs2zWj4RVys1"
-// 	PlexOnlineUsername="Timmayy"
-// 	PlexOnlineMail="timjfernandes@gmail.com"
-// 	PlexOnlineHome="1"
-// 	DlnaEnabled="0"
-// 	DvrIncrementalEpgLoader="0"
-// 	CertificateUUID="bf3439507c5b413cabb13e0fc4cc41dd"
-// 	PubSubServer="184.105.148.113"
-// 	PubSubServerRegion="sjc"
-// 	PubSubServerPing="31"
-// 	CertificateVersion="3"
-// 	LastAutomaticMappedPort="0"
-// 	ManualPortMappingMode="1"
-// 	LanguageInCloud="1"
-// />
-
 pub static CONFIG_PATH: &str = "../app/config.json";
 
 
