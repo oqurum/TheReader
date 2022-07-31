c4f5aeff266ae82794eade88738bc2e28923a3bf
Its <2779546+Its-its@users.noreply.github.com>
2022-07-30 18:55:43 -0700

    Import global common SCSS into public Remove local duplicate SCSS files

---
+++ crates/frontend/build.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+fn main() {
+    let contents = grass::from_path(
+        "../../global_common/scss/main.scss",
+        &grass::Options::default()
+    ).expect("Unable to parse SCSS");
+
+    fs::write(
+        "../../app/public/css/gcommon.css",
+        contents
+    ).expect("Failed writing to file");
+}
