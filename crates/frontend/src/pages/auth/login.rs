c725c30913eccd283d4a285551485d96ad735725
Its <2779546+Its-its@users.noreply.github.com>
2022-08-02 15:12:59 -0700

    Fix yew login states

--- crates/frontend/src/pages/auth/login.rs
+++ crates/frontend/src/pages/auth/login.rs
@@ -1,6 +1,6 @@
 use common::api::ApiErrorResponse;
 use web_sys::HtmlInputElement;
-use yew::prelude::*;
+use yew::{prelude::*, html::Scope};
 use yew_router::{prelude::RouterScopeExt, history::History};
 
 use crate::{request, Route};
@@ -53,78 +53,108 @@ impl Component for LoginPage {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let passless_email = use_state(String::new);
-
-        let pass_email = use_state(String::new);
-        let pass_pass = use_state(String::new);
-
-        let on_change_passless_email = {
-            let value = passless_email.setter();
-            Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
-        };
-
-        let submit_passless = {
-            ctx.link().callback_future(move |_| {
-                let email = passless_email.clone();
-
-                async move {
-                    let resp = request::login_without_password(email.to_string()).await;
-
-                    Msg::LoginPasswordlessResponse(resp.ok())
-                }
-            })
-        };
-
-
-        let on_change_pass_email = {
-            let value = pass_email.setter();
-            Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
-        };
-
-        let on_change_pass_pass = {
-            let value = pass_pass.setter();
-            Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
-        };
-
-        let submit_pass = {
-            ctx.link().callback_future(move |_| {
-                let email = pass_email.clone();
-                let pass = pass_pass.clone();
-
-                async move {
-                    let resp = request::login_with_password(email.to_string(), pass.to_string()).await;
-
-                    Msg::LoginPasswordResponse(resp.ok())
-                }
-            })
-        };
-
-
         html! {
             <div class="login-container">
                 <div class="center-normal">
                     <div class="center-container">
-                        <h2>{ "Passwordless Login" }</h2>
-                        <div class="form-container">
-                            <label for="email">{ "Email Address" }</label>
-                            <input type="email" name="email" id="email" onchange={ on_change_passless_email } />
-
-                            <input type="submit" value="Log in" class="button" onclick={ submit_passless } />
-                        </div>
-
-                        <h2>{ "Password Login" }</h2>
-                        <div class="form-container">
-                            <label for="email">{ "Email Address" }</label>
-                            <input type="email" name="email" id="email" onchange={ on_change_pass_email } />
-
-                            <label for="password">{ "Password" }</label>
-                            <input type="password" name="password" id="password" onchange={ on_change_pass_pass } />
-
-                            <input type="submit" value="Log in" class="button" onclick={ submit_pass } />
-                        </div>
+                        <PasswordlessLogin cb={ ctx.link().clone() } />
+                        <PasswordLogin cb={ ctx.link().clone() } />
                     </div>
                 </div>
             </div>
         }
+    }
+}
+
+
+#[derive(Properties)]
+struct InnerProps {
+    cb: Scope<LoginPage>,
+}
+
+impl PartialEq for InnerProps {
+    fn eq(&self, _other: &Self) -> bool {
+        false
+    }
+}
+
+
+
+#[function_component(PasswordlessLogin)]
+fn passwordless(props: &InnerProps) -> Html {
+    let passless_email = use_state(String::new);
+
+    let on_change_passless_email = {
+        let value = passless_email.setter();
+        Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
+    };
+
+    let submit_passless = {
+        props.cb.callback_future(move |_| {
+            let email = passless_email.clone();
+
+            async move {
+                let resp = request::login_without_password(email.to_string()).await;
+
+                Msg::LoginPasswordlessResponse(resp.ok())
+            }
+        })
+    };
+
+    html! {
+        <>
+            <h2>{ "Passwordless Login" }</h2>
+            <div class="form-container">
+                <label for="email">{ "Email Address" }</label>
+                <input type="email" name="email" id="email" onchange={ on_change_passless_email } />
+
+                <input type="submit" value="Log in" class="button" onclick={ submit_passless } />
+            </div>
+        </>
+    }
+}
+
+
+#[function_component(PasswordLogin)]
+fn password(props: &InnerProps) -> Html {
+    let pass_email = use_state(String::new);
+    let pass_pass = use_state(String::new);
+
+    let on_change_pass_email = {
+        let value = pass_email.setter();
+        Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
+    };
+
+    let on_change_pass_pass = {
+        let value = pass_pass.setter();
+        Callback::from(move |e: Event| value.set(e.target_unchecked_into::<HtmlInputElement>().value()))
+    };
+
+    let submit_pass = {
+        props.cb.callback_future(move |_| {
+            let email = pass_email.clone();
+            let pass = pass_pass.clone();
+
+            async move {
+                let resp = request::login_with_password(email.to_string(), pass.to_string()).await;
+
+                Msg::LoginPasswordResponse(resp.ok())
+            }
+        })
+    };
+
+    html! {
+        <>
+            <h2>{ "Password Login" }</h2>
+            <div class="form-container">
+                <label for="email">{ "Email Address" }</label>
+                <input type="email" name="email" id="email" onchange={ on_change_pass_email } />
+
+                <label for="password">{ "Password" }</label>
+                <input type="password" name="password" id="password" onchange={ on_change_pass_pass } />
+
+                <input type="submit" value="Log in" class="button" onclick={ submit_pass } />
+            </div>
+        </>
     }
 }
