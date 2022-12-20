00bcdbbbec3f3d07b5b05feef989eef508ffb28c
Its <2779546+Its-its@users.noreply.github.com>
2022-12-20 01:53:57 -0800

    Overwrite /app/public/dist when changed

--- crates/backend/src/bundle.rs
+++ crates/backend/src/bundle.rs
@@ -6,8 +6,22 @@ static PUBLIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR../../../app/public");
 
 // TODO: Use a virtual directory?
 
+// TODO: Improve. Very basic.
+
 pub async fn export() -> Result<()> {
-    std::fs::create_dir_all("./app/public")?;
+    std::fs::create_dir_all("./app/public/dist");
+
+    for entry in std::fs::read_dir("./app/public/dist")? {
+        let entry = entry?;
+
+        if !PUBLIC_DIR.contains(format!("dist/{}", entry.file_name().to_str().unwrap())) {
+            tracing::info!("Removing Existing /dist Folder");
+
+            std::fs::remove_dir_all("./app/public/dist")?;
+
+            break;
+        }
+    }
 
     PUBLIC_DIR.extract("./app/public")?;
 
